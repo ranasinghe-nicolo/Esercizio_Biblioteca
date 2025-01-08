66a446b91b5eb7b90e0ed71b5aac248bb304029d
Ranasinghe Nicolo <148004148+ranasinghe-nicolo@users.noreply.github.com>
2025-01-08 17:59:02 +0100

    creazione dizionario utenti_bibliioteca

---
+++ main.py
@@ -0,0 +1,29 @@
+utenti_biblioteca = [
+    {
+        "nome":"Nick",
+        "cognome":"Rana",
+        "libriPrestati": ["libro1","libro2","libro2"]
+    },
+
+    {
+        "nome":"Gaetano",
+        "cognome":"Za",
+        "libriPrestati": ["libro1","libro2","libro2"]
+    },
+
+    {
+        "nome":"Nick2",
+        "cognome":"Rana2",
+        "libriPrestati": ["libro1","libro2","libro2"]
+    },
+
+    {
+        "nome":"Gaetano2",
+        "cognome":"Za2",
+        "libriPrestati": ["libro1","libro2","libro2"]
+    },
+   
+]
+
+
+##
